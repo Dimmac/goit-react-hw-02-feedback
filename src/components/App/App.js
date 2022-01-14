93774a69a0aa362918e2a019638c97358a77c8ef
Dimmac <dmytro.lojivsky@gmail.com>
2022-01-14 07:58:57 +0200

    render

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Feedback } from '../Feedback/Feedback.jsx';
+import { Container } from './App.styled';
+
+function App() {
+  return (
+    <Container>
+      <Feedback />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+export const Container = styled.div`
+  background-color: #d0f9ff73;
+  padding: 0 50px;
+`;
