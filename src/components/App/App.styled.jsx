a9c073d7008ee90be2120043b0d9cbc3b5600d53
Dimmac <dmytro.lojivsky@gmail.com>
2022-01-22 00:35:58 +0200

    task_1

--- src/components/App/App.styled.jsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-export const Container = styled.div`
-  background-color: #d0f9ff73;
-  padding: 0 50px;
-`;
--- src/components/Feedback/Feedback.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Button = styled.button`
-  background-color: aqua;
-  padding: 5px;
-  margin: 5px;
-  border-radius: 4px;
-  border-color: black;
-`;
